e500a6a7b4d58f085d36960791c8d78600a1a27c
Raisul Islam <raisul.rana16@gmail.com>
2025-03-08 01:30:23 +0600

    Uploading math and operator

---
+++ variable/math_operator.js
@@ -0,0 +1,60 @@
+// Practice 1
+
+const income = 100;
+const expense = 40;
+
+const remaining = income - expense;
+
+console.log("Remaining Balance: ", remaining);
+
+// Practice 2
+
+const totalPencils = 10;
+const useEachTime = 2;
+
+const usingTimes = totalPencils / useEachTime;
+
+console.log(usingTimes, "times");
+
+// Practice 3
+
+const availableMoney = 60;
+const expenseEachTime = 15;
+
+const moneyUsingTimes = availableMoney / expenseEachTime;
+const totalExpense = expenseEachTime * moneyUsingTimes;
+
+const remainingBalance = availableMoney - totalExpense;
+
+console.log(moneyUsingTimes, "times");
+console.log(totalExpense, "money");
+console.log("Remaining Balance: ", remainingBalance);
+
+// Practice 4
+
+const stringOne = "Hello";
+const stringTwo = "World";
+
+const concatString = stringOne + stringTwo;
+console.log(concatString);
+
+// Practice 5
+
+const rice = 153;
+const lentil = 261;
+
+const totalWeight = rice + lentil;
+
+console.log(totalWeight)
+
+// Practice 6
+
+const totalBalance = 500;
+const expenseEachTime2 = 75;
+const expenseTimes = totalBalance / expenseEachTime2;
+
+console.log(expenseTimes, "times");
+
+const remainingBalance2 = totalBalance - (expenseEachTime2 * expenseTimes);
+
+console.log("Remaining Balance: ", remainingBalance2);
